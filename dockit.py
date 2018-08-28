1a08a74a9c87c0eaa11e11d8f85dee68b2c0a216
Jim Carr <restricted>
2018-08-27 21:47:37 -0400

    First working version.

--- dockit.py
+++ dockit.py
@@ -1,19 +1,52 @@
 #!/usr/bin/python3
 
+import os
 import json
+import sys
 
-with open('dockit.json') as json_data:
-	configData = json.load(json_data)
+class CDockerMgr:
+	def __init__(self):
+		with open('dockit.json') as json_data:
+			self.configData = json.load(json_data)
+			self.dockerCmd = self.configData["dockercommand"]
 
-	print("Docker command is '{0}'\n".format(configData["dockercommand"]))
-	
-	print("Base commands:\n")
-	for childkey,childvalue in configData["commands"]["base"].items():
-		print(" {0} -> {1}".format(childkey,childvalue))
-	print("")
-	
-	print("Container commands:\n")
-	for containerKey in configData["commands"]["containers"]:
-		for childkey,childvalue in configData["commands"]["containers"][containerKey].items():
-			print(" {0}.{1} -> {2}".format(containerKey,childkey,childvalue))
+	def ExecuteBaseCommand(self, currentCommand):
+		for childkey,childvalue in self.configData["commands"]["base"].items():
+			if childkey == currentCommand:
+				self.ExecDocker(childvalue)
+		self.ShowHelp()
+
+	def ExecuteContainerCommand(self, currentContainer, currentCommand):
+		for containerKey in self.configData["commands"]["containers"]:
+			if containerKey == currentContainer:
+				for childkey,childvalue in self.configData["commands"]["containers"][containerKey].items():
+					if childkey == currentCommand:
+						self.ExecDocker(childvalue)
+		self.ShowHelp()
+
+	def ExecDocker(self, args):
+		fullCmd = "{0} {1}".format(self.dockerCmd, args)
+		os.system(fullCmd)
+		exit(0)
+
+	def ShowHelp(self):
+		print("USAGE:")
+		print("\tdockit <container> <command>")
 		print("")
+		print("Valid containers and commands:")
+		for childkey,childvalue in self.configData["commands"]["base"].items():
+			print("\t{0}".format(childkey))
+		for containerKey in self.configData["commands"]["containers"]:
+			print("\t{0}".format(containerKey))
+			for childkey,childvalue in self.configData["commands"]["containers"][containerKey].items():
+				print("\t\t{0}".format(childkey))
+		exit(0)
+
+myDockerMgr = CDockerMgr()
+
+if len(sys.argv) == 2:
+	myDockerMgr.ExecuteBaseCommand(sys.argv[1])
+elif len(sys.argv) == 3:
+	myDockerMgr.ExecuteContainerCommand(sys.argv[1], sys.argv[2])
+else:
+	myDockerMgr.ShowHelp()
